f9214093a2a5e7fdd914c8c6700bb5072ddee4d7
ChaonanShen <1686745041@qq.com>
2025-10-06 09:15:09 +0800

    finish algorithm4, 算法不难，就是各种类型各种借用规则得弄对，一定要把vscode配置成能够显示类型和做基本检查的

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -1,12 +1,11 @@
 /*
-	binary_search tree
-	This problem requires you to implement a basic interface for a binary tree
+    binary_search tree
+    This problem requires you to implement a basic interface for a binary tree
 */
 
 //I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
-
 
 #[derive(Debug)]
 struct TreeNode<T>
@@ -43,20 +42,57 @@ impl<T> BinarySearchTree<T>
 where
     T: Ord,
 {
-
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
 
     // Insert a value into the BST
+    // 不断左右找，找到应插入节点，然后调用TreeNode::insert插入
     fn insert(&mut self, value: T) {
-        //TODO
+        match self.root.as_mut() {
+            None => {
+                self.root = Some(Box::new(TreeNode::new(value)));
+            },
+            Some(root_node) => {
+                root_node.insert(value);
+            }
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        match self.root.as_ref() {
+            None => {
+                false
+            },
+            Some(mut root_node) => {
+                loop {
+                    let  root_value = &root_node.value;
+                    if *root_value == value {
+                        return true;
+                    } else if *root_value < value {
+                        match root_node.left.as_ref() {
+                            None => {
+                                return false;
+                            },
+                            Some(root_left_value) => { // 继续向左子树搜索
+                                root_node = root_left_value;
+                            },
+                        }
+                    } else {
+                        match root_node.right.as_ref() {
+                            None => {
+                                return false;
+                            },
+                            Some(root_right_value) => {
+                                root_node = root_right_value;
+                            }
+                        }
+                    }
+                }
+            },
+        }
     }
 }
 
@@ -65,11 +101,33 @@ where
     T: Ord,
 {
     // Insert a node into the tree
-    fn insert(&mut self, value: T) {
+    pub fn insert(&mut self, value: T) {
         //TODO
+        let node_value = &self.value;
+        if *node_value == value {
+            // 不允许重复插入，直接返回
+            return;
+        } if *node_value < value { // 插入左子树
+            match self.left.as_mut() {
+                None => { // 没有左子树，直接插入
+                    self.left = Some(Box::new(TreeNode::new(value)));
+                },
+                Some(left_node) => { // 递归插入左子树
+                    left_node.insert(value);
+                }
+            }
+        } else { // 插入右子树
+            match self.right.as_mut() {
+                None => {
+                    self.right = Some(Box::new(TreeNode::new(value)));
+                },
+                Some(right_node) => {
+                    right_node.insert(value);
+                }
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
